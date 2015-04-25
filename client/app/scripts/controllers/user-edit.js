c794a0a5595b7f05d9f9bb94c3204af073a0922d
razvanilin <razvanilin@gmail.com>
2015-04-25 15:55:40 +0100

    resolve #16 - The password can now be changed separately from the profile in the user edit view

--- client/app/scripts/controllers/user-edit.js
+++ client/app/scripts/controllers/user-edit.js
@@ -10,19 +10,49 @@
 angular.module('clientApp')
   .controller('UserEditCtrl', function ($scope, $routeParams, $http, User, $window, $location) {
     $scope.viewUser = true;
-    $scope.user = {};
+    $scope.profile = {};
+    $scope.password = {};
     if ($window.sessionStorage.user) {
         var id = JSON.parse($window.sessionStorage.user).id;
         User.one(id).get().then(function(data, status, headers, config) {
-            $scope.user = data;
-            $scope.saveUser = function() {
-                $scope.user.save().then(function() {
+            var profile = data;
+            delete profile.password;
+            $scope.profile = profile;
+
+            $scope.saveProfile = function() {
+                $scope.profile.save().then(function() {
                     $location.path('/user/'+$routeParams.username);
                 });
             };
+
+            $scope.password             = profile;
+            $scope.password.old         = "";
+            $scope.password.new         = "";
+            $scope.password.newConfirm  = "";
+            $scope.password.error       = false;
+           
+
+            $scope.changePassword = function() {
+                if ($scope.password.new != $scope.password.newConfirm) {
+                    $scope.password.error = true;
+                } else {
+                    $scope.password.error = false;
+                }
+
+                if (!$scope.password.error && $scope.password.new.length > 0) {
+                    User.one($scope.password.username).customPUT($scope.password, 'password').then(function(data){
+                        if (data.status === 401) {
+                            $location.path('/user/'+$routeParams.username+'/edit');
+                        }
+                        $location.path('/user/'+$routeParams.username);
+                    });
+                }
+            }
+            
         });
     } else {
         $location.path('/login');
+
     }
     /*$scope.user.username = $routeParams.username;
     $http({
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,22 +7,31 @@ module.exports = function(app, route) {
 
     // Setup the controller for REST
     var User = restful.model(
-        'user',
-        app.models.user
-    ).methods(['get', 'put', 'post', 'delete'])
-    .before('get', expressJwt({secret: app.settings.secret}))
-    .before('post', hash_password);
-    //.before('put', hash_password);
+            'user',
+            app.models.user
+        ).methods(['get', 'put', 'post', 'delete'])
+        // uncomment this in production
+        //.before('get', expressJwt({ secret: app.settings.secret }))
+        .before('post', hash_password)
+        .before('put', expressJwt({
+            secret: app.settings.secret
+        }), checkForPassword);
+
+
+    function checkForPassword(req, res, next) {
+        if (req.body.password) return res.status(400).send("Bad request");
+
+        next();
+    }
 
     function hash_password(req, res, next) {
 
         // First check if all the required fields are not empty
-        if (req.body.username == null || req.body.email == null || req.body.password == null
-            || req.body.username == '' || req.body.email == '' || req.body.password == '') {
+        if (req.body.username == null || req.body.email == null || req.body.password == null || req.body.username == '' || req.body.email == '' || req.body.password == '') {
 
             return res.status(400).send('Incorrect data');
         }
-        console.log("username: "+req.body.username+" email: "+req.body.email);
+        console.log("username: " + req.body.username + " email: " + req.body.email);
         bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
             if (err) return next(err);
 
@@ -47,6 +56,55 @@ module.exports = function(app, route) {
                 return res.status(404).send("User not found");
             else
                 res.send(data.profile);
+        });
+    });
+
+
+    // Change password route
+    app.put('/user/:username/password', expressJwt({secret: app.settings.secret}), function(req, res, next) {
+        var oldPassword = req.body.old;
+        var newPassword = req.body.new;
+        var username = req.params.username;
+
+        if (oldPassword == '' || newPassword == '' || username == '') {
+            return res.status(400).send('Bad Request');
+        }
+
+        User.findOne({
+            username: username
+        }, function(err, user) {
+            if (err || user === null) {
+                return res.status(404).send("User not found");
+            }
+            
+            user.comparePassword(oldPassword, function(isMatch) {
+                if (!isMatch) {
+                    return res.status(401).send("Wrong password");
+                }
+
+                // if everything is fine, assign the new password to the password field
+                req.body.password = newPassword;
+
+                // hash the password
+                bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+                    if (err) return next(err);
+
+                    bcrypt.hash(req.body.password, salt, null, function(err, hash) {
+                        if (err) return next(err);
+                        user.password = hash;
+                    });
+                });
+
+                // save the new password
+                user.save(function(err) {
+                    if (err) {
+                        return res.status(400).send("Could not update");
+                    } else {
+                        return res.status(200).send("User Updated");
+                    }
+                });
+
+            });
         });
     });
 
