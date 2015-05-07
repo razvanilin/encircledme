ff6296603c019afc33103a8f1181f7fec17f7ba3
razvanilin <razvanilin@gmail.com>
2015-05-07 22:17:33 +0100

    resolve $48 - Modified the User model after the same principle applied in the CSS file - also fixed the default picture's path in the model

--- server/models/User.js
+++ server/models/User.js
@@ -20,7 +20,7 @@ var UserSchema = new mongoose.Schema({
     profile: {
         avatar: {
             type: String,
-            default: 'default.png'
+            default: '/uploads/default.png'
         },
         firstname: {
             type: String,
@@ -29,6 +29,44 @@ var UserSchema = new mongoose.Schema({
         lastname: {
             type: String,
             default: 'Name'
+        },
+        social: {
+            top: {
+                network: {type: String, default: 'instagram'},
+                link: {type: String, default: 'http://instagram.com/'}
+            },
+            topLeft: {
+                network: {type: String, default: 'twitter'},
+                link: {type:String, default: 'http://twitter.com/'}
+            },
+            topRight: {
+                network: {type:String, default: 'facebook'},
+                link: {type:String, default: 'http://facebook.com/'}
+            },
+            left: {
+                network: {type:String, default: 'youtube'},
+                link: {type:String, default: 'http://youtube.com/'}
+            },
+            right: {
+                network: {type:String, default: 'google'},
+                link: {type:String, default: 'http://plus.google.com/'}
+            },
+            bottom: {
+                network: {type:String, default: 'flickr'},
+                link: {type:String, default: 'http://flickr.com/'}
+            },
+            bottomLeft: {
+                network: {type:String, default: 'github'},
+                link: {type:String, default: 'http://github.com'}
+            },
+            bottomRight: {
+                network: {type:String, default: 'linkedin'},
+                link: {type:String, default: 'http://linkedin.com'}
+            },
+            centre: {
+                network: {type:String, default: 'email'},
+                link: {type:String, default: 'user@email.com'}
+            }
         }
     },
     uploads: []
