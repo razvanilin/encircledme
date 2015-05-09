de5491c7907d274b1ce5cb5f3376bb1af7f949f9
razvanilin <razvanilin@gmail.com>
2015-05-09 11:48:53 +0100

    re #50 re #49 - Improved the User model to contain a position attribute and also modified the view with the correct models and buttons

--- server/models/User.js
+++ server/models/User.js
@@ -32,50 +32,60 @@ var UserSchema = new mongoose.Schema({
         },
         social: {
             top: {
+                position: {type: String, enum: ['top'], default: 'top'},
                 network: {type: String, default: 'instagram'},
                 link: {type: String, default: 'http://instagram.com/'},
                 logo: {type: String, default: '/uploads/instagram.png'}
             },
             topLeft: {
+                position: {type: String, enum: ['topLeft'], default: 'topLeft'},
                 network: {type: String, default: 'twitter'},
                 link: {type:String, default: 'http://twitter.com/'},
                 logo: {type: String, default: '/uploads/twitter.png'}
             },
             topRight: {
+                position: {type: String, enum: ['topRight'], default: 'topRight'},
                 network: {type:String, default: 'facebook'},
                 link: {type:String, default: 'http://facebook.com/'},
                 logo: {type: String, default: '/uploads/facebook.png'}
             },
             left: {
+                position: {type: String, enum: ['left'], default: 'left'},
                 network: {type:String, default: 'youtube'},
                 link: {type:String, default: 'http://youtube.com/'},
                 logo: {type: String, default: '/uploads/youtube.png'}
             },
             right: {
+                position: {type: String, enum: ['right'], default: 'right'},
                 network: {type:String, default: 'google'},
                 link: {type:String, default: 'http://plus.google.com/'},
                 logo: {type: String, default: '/uploads/google.png'}
             },
             bottom: {
+                position: {type: String, enum: ['bottom'], default: 'bottom'},
                 network: {type:String, default: 'flickr'},
                 link: {type:String, default: 'http://flickr.com/'},
                 logo: {type: String, default: '/uploads/flickr.png'}
             },
             bottomLeft: {
+                position: {type: String, enum: ['bottomLeft'], default: 'bottomLeft'},
                 network: {type:String, default: 'github'},
                 link: {type:String, default: 'http://github.com'},
                 logo: {type: String, default: '/uploads/github.png'}
             },
             bottomRight: {
+                position: {type: String, enum: ['bottomRight'], default: 'bottomRight'},
                 network: {type:String, default: 'linkedin'},
                 link: {type:String, default: 'http://linkedin.com'},
                 logo: {type: String, default: '/uploads/linkedin.png'}
             },
             centre: {
+                position: {type: String, enum: ['centre'], default: 'centre'},
                 network: {type:String, default: 'email'},
                 link: {type:String, default: 'user@email.com'},
                 logo: {type: String, default: '/uploads/email.png'}
             }
+
         }
     },
     uploads: []
