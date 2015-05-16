769b8597c58eea02a2dae3a9f0b2ad309863da67
razvanilin <razvanilin@gmail.com>
2015-05-16 16:10:17 +0100

    resolve #49 - Added support for activation/deactivation of networks

--- client/app/scripts/controllers/social-links.js
+++ client/app/scripts/controllers/social-links.js
@@ -8,70 +8,84 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-    .controller('SocialLinksCtrl', function($scope, $window, User, CONFIG, $location, $anchorScroll) {
-        $scope.viewNetworks = true;
-        $scope.isMaxNetworks = false;
-        $scope.viewEdit = false;
-        $scope.viewEditError = false;
+    .controller('SocialLinksCtrl', function($scope, $window, User, CONFIG, $location, $anchorScroll, AuthenticationService) {
 
-        $scope.profile = {};
-        $scope.selectedNetwork = {};
+        if (AuthenticationService.isLogged) {
+            $scope.viewNetworks = true;
+            $scope.isMaxNetworks = false;
+            $scope.viewEdit = false;
+            $scope.viewEditError = false;
 
-        var username = JSON.parse($window.sessionStorage.user).username;
-        var id = JSON.parse($window.sessionStorage.user).id;
-        User.one(username).get().then(function(data) {
-            console.log(data);
-            $scope.profile = data;
-        }, function(response) {
-            console.log(response);
-        });
+            $scope.profile = {};
+            $scope.selectedNetwork = {};
 
-        $scope.beginEdit = function(position) {
-
-            $scope.selectedNetwork = $scope.profile.social[position];
-            console.log($scope.selectedNetwork);
-
-            $scope.viewEdit = true;
-            setTimeout(function() {
-                var old = $location.hash();
-                $location.hash('editNetwork');
-                $anchorScroll();
-                //reset to old to keep any additional routing logic from kicking in
-                $location.hash(old);
-
+            var username = JSON.parse($window.sessionStorage.user).username;
+            var id = JSON.parse($window.sessionStorage.user).id;
+            User.one(username).get().then(function(data) {
+                console.log(data);
+                $scope.profile = data;
+            }, function(response) {
+                console.log(response);
             });
-        };
 
-        $scope.editNetwork = function() {
-            $scope.profile.social[$scope.selectedNetwork.position] = $scope.selectedNetwork;
-            console.log($scope.profile.social[$scope.selectedNetwork.position]);
+            $scope.beginEdit = function(position) {
 
-            User.one(username).customPUT($scope.profile, 'network').then(function(data) {
-                console.info(data);
+                $scope.selectedNetwork = $scope.profile.social[position];
+                console.log($scope.selectedNetwork);
 
-                $scope.viewEdit = false;
-
-                // scroll back to the top of the page
+                $scope.viewEdit = true;
                 setTimeout(function() {
                     var old = $location.hash();
-                    $location.hash('manageNetworks');
+                    $location.hash('editNetwork');
                     $anchorScroll();
                     //reset to old to keep any additional routing logic from kicking in
                     $location.hash(old);
 
                 });
+            };
 
-            }, function(response) {
-                console.error(response);
-                $scope.viewEditError = true;
-            });
-        };
+            $scope.editNetwork = function() {
+                $scope.profile.social[$scope.selectedNetwork.position] = $scope.selectedNetwork;
+                console.log($scope.profile.social[$scope.selectedNetwork.position]);
 
-        $scope.addNetwork = function() {
-            console.log($scope.profile.social);
-            if (Object.keys($scope.profile.social).length >= CONFIG.MAX_NETWORKS) {
-                $scope.isMaxNetworks = true;
-            }
-        };
+                User.one(username).customPUT($scope.profile, 'network').then(function(data) {
+                    console.info(data);
+
+                    $scope.viewEdit = false;
+
+                    // scroll back to the top of the page
+                    setTimeout(function() {
+                        var old = $location.hash();
+                        $location.hash('manageNetworks');
+                        $anchorScroll();
+                        //reset to old to keep any additional routing logic from kicking in
+                        $location.hash(old);
+
+                    });
+
+                }, function(response) {
+                    console.error(response);
+                    $scope.viewEditError = true;
+                });
+            };
+
+            $scope.addNetwork = function() {
+                console.log($scope.profile.social);
+                if (Object.keys($scope.profile.social).length >= CONFIG.MAX_NETWORKS) {
+                    $scope.isMaxNetworks = true;
+                }
+            };
+
+            $scope.test = function() {
+                User.one(username).customPUT($scope.profile, 'network').then(function(data) {
+                    console.info(data);
+
+                }, function(response) {
+                    console.error(response);
+                });
+            };
+        } else {
+        	$location.path('login');
+        }
 
     });
