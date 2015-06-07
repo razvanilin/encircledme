8fd184cd6e14fc3fd2625ab1ee6644ee5f316801
razvanilin <razvanilin@gmail.com>
2015-06-07 17:50:33 +0100

    the user controller in the angular app now check for email type networks and applies mailto to them

--- client/app/scripts/controllers/user.js
+++ client/app/scripts/controllers/user.js
@@ -21,6 +21,14 @@ angular.module('clientApp')
         });
 
     $scope.accessNetwork = function(network) {
-    	$window.open(network);
+        // check to see if the link is an email and apply 'mailto' to it
+        if (validateEmail(network)) $window.location = 'mailto:'+network;
+        
+        else $window.open(network);
     };
+
+    function validateEmail(email) { 
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
   });
